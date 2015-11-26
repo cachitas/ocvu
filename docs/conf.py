107b2ed26285eb09e4bbf3d5961d3cad31273642
Hugo Cachitas <hcachitas@gmail.com>
2015-11-26 16:30:03 +0000

    Add pandas do docs conf file

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import sphinx_rtd_theme
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
-MOCK_MODULES = ['numpy', 'cv2']
+MOCK_MODULES = ['numpy', 'cv2', 'pandas']
 for mod_name in MOCK_MODULES:
    sys.modules[mod_name] = mock.Mock()
 
